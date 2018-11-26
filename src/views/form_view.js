ac4ad8416db4f09277f161d77d1e2358bbac2069
Jake Thwaites <jakethwaites@hotmail.co.uk>
2018-11-26 20:26:11 +0000

    created form_view

---
+++ src/views/form_view.js
@@ -0,0 +1,18 @@
+
+
+const FormView = function() {
+
+};
+
+FormView.prototype.bindEvents = function () {
+  const form = document.querySelector("#prime-checker-form")
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const inputtedNumber = event.target.number.value;
+
+    console.log("Inputted number:" inputtedNumber);
+  })
+};
+
+
+module.exports = FormView;
